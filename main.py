ede68e1dcf9dc85f62917d8d2b15c7c5b2765efa
enselme <eaguidigodo@gmail.com>
2025-03-15 12:03:20 +0100

    Completed data insertion into the database.

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import pandas as pd 
 import pg8000
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
-#df = pd.read_excel('people sample.xlsx')
-#print(type(df))
 
-
-def connect_to_db():
+def connect_to_db(data=None):
     """ Connect to databse. """
     # Load environment variables from .env file
     load_dotenv()
-    print(f"DB_PORT: {os.getenv('DB_PASSWORD')}")
     # Database connection details
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
@@ -28,13 +25,65 @@ def connect_to_db():
             password=DB_PASSWORD,
             port=DB_PORT
         )
-        print("Connection successful!")
+        print("Connection successful! We will now create the table that will store your data....")
     except Exception as e:
         print("Sorry, we are unable to connect to your database:", e)
+    else:
+        cursor = connection.cursor()
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS people (
+                       id SERIAL PRIMARY KEY,
+                       matricule VARCHAR(255),
+                       nom VARCHAR(255), 
+                       prenom VARCHAR(50),
+                       datedenaissance VARCHAR(255),
+                       status VARCHAR(255)
+                    )
+                       """
+        )
+        print("Table created!")
+        print("It's almost done! It's the last step. Your data is being inserted into a database. This may take some time. Wait please until it's completed....")
+        if data is not None:
+            query = "INSERT INTO people (matricule, nom, prenom, datedenaissance, status) VALUES (%s, %s, %s, %s, %s)"
+            cursor.executemany(query, data)
+            connection.commit()
+            return cursor.rowcount
     finally:
         if connection:
             connection.close()
 
+# def standardise_date(date_str):
+#     date_obj = datetime.strptime(date_str, "%d/%m/%Y")
+#     date_pg = date_obj.strftime("%Y-%m-%d")
+#     return date_pg
+
+def import_data(xls_file_path):
+    """ Import data from excel file to database. """
+    print(f"We are reading {xls_file_path} that you provide us...")
+    try:
+        df = pd.read_excel('people sample.xlsx')
+        expected_columns = {"matricule","nom","prenom","datedenaissance","status"}
+        if not expected_columns.issubset(df.columns):
+            print("Sorry, seems that your file does not contains expected columns.")
+            return None
+        people = []
+        print("We retrived your data. We are preparing them to insert them into the database.")
+        for _, row in df.iterrows():
+            people.append((row["matricule"], row["nom"], row["prenom"], row["datedenaissance"], row["status"]))
+        print("Everything is going smoothly. Now we are about to connect to your database....")
+        rowcount = connect_to_db(people)
+        print(f"{rowcount} rows successful added.")
+    except Exception as e:
+        print("Sorry, we got an error while importing data from your files.", e)
+        return None
+        
+    #print(type(df))
+
+
+
 if __name__=="__main__":
-    connect_to_db()
+    xls_file_path = 'people sample.xlsx'
+    import_data(xls_file_path)
+    #df = pd.read_excel('people sample.xlsx')
+    #print(df)
 
